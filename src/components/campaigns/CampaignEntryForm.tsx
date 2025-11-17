0366e56fb3047f81002e96d17d38324e539e4dcd
[dyad] <git@dyad.sh>
2025-11-17 15:01:19 +0530

    [dyad] Fixed `useForm` import error - wrote 1 file(s)

--- src/components/campaigns/CampaignEntryForm.tsx
+++ src/components/campaigns/CampaignEntryForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { useForm } from "@hookform/react";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { Button } from "@/components/ui/button";
